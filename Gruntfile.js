cb786647ab785cdb4fc00712607045155064d886
ejb <mail@elliotbentley.com>
2015-06-23 15:45:13 +0100

    Initial public commit

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {   
+            js: {
+                src: ['src/js/*.js'],
+                dest: 'dist/pinpoint.js'
+            }
+            
+        },
+        
+        uglify: {
+            build: {
+                src: 'dist/pinpoint.js',
+                dest: 'dist/pinpoint.min.js'
+            }
+        },
+        
+        sass: {
+            dist: {
+                files: {
+                    'dist/pinpoint.css': 'src/scss/main.scss'
+                }
+            },
+            dist_min: {
+                options: {
+                    style: 'compressed',
+                    sourcemap: 'none'
+                },
+                files: {
+                    'dist/pinpoint.min.css': 'src/scss/main.scss'
+                }
+            }
+        },
+        
+        watch: {
+            options: {
+                livereload: true,
+            },
+            scripts: {
+                files: ['src/js/*'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false,
+                },
+            },
+            css: {
+                files: ['src/scss/*'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                }
+            }
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+	
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['concat','uglify','sass']);
+
+};
