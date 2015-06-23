475341ae5c957c256be12a4cc72660193de9d2b3
ejb <mail@elliotbentley.com>
2015-06-23 15:45:13 +0100

    Initial public commit

---
+++ Readme.md
@@ -0,0 +1,151 @@
+# Pinpoint
+
+A JavaScript library for creating beautifully simple maps in seconds. Developed at The Wall Street Journal.
+
+- [Example on WSJ.com](http://www.wsj.com/articles/mass-grave-of-boko-haram-victims-discovered-1426867025)
+- [Blog post](http://ejb.github.io/2015/03/21/pinpoint-maps-at-wsj.html)
+
+## Features
+
+- Map settings are stored in a simple JSON format and can be generated using [Pinpoint Editor](https://github.com/dowjones/pinpoint-editor).
+- Fully responsive with touch-friendly "click-to-activate" behaviour.
+- Powered by the awesome [Leaflet.js](http://leafletjs.com).
+
+## Quickstart
+
+Include `pinpoint.js` and `pinpoint.css` on your page.
+
+In terms of HTML, just a single div is needed:
+
+```html
+<div class="map-container"></div>
+```
+
+Then in your JavaScript file:
+
+```js
+var options = {
+    "element": ".map-container",
+    "aspect-ratio": "wide",
+    "dek": "",
+    "hed": "The U.K.",
+    "lat": 51.5049378,
+    "lon": -0.0870377,
+    "minimap": false,
+    "zoom": 4,
+    "markers": [
+        {
+            "lat": 51.5049378,
+            "lon": -0.0870377,
+            "text": "London"
+        }
+    ]
+};
+var map = new Pinpoint( options );
+```
+
+
+### Options
+
+Pinpoint accepts a JavaScript object for configuration.
+
+option              | type     | default                                    | description
+------------------- | -------- | ------------------------------------------ | ------------------
+element             | string   | "#map-el"                                  | CSS selector for map parent element.
+aspect-ratio        | string   | (required)                                 | The shape of the map: "wide" (3x2), "square" (1x1), or "tall" (5x6)
+lat                 | number   | (required)                                 | Latitude for map centre.
+lon                 | number   | (required)                                 | Longitude for map centre.
+markers             | array    | (required)                                 | Array of markers/icons on map (see below for details).
+zoom                | number   | (required)                                 | Initial zoom level of map.
+hed                 | string   | (blank)                                    | Headline for map (optional).
+dek                 | string   | (blank)                                    | Dek for map (optional).
+note                | string   | (blank)                                    | Note to go below map (optional).
+minimap             | boolean  | false                                      | Set to `true` to enables zoomed-out smaller map in corner.
+minimap-zoom-offset | number   | -5                                         | Zoom level of minimap relative to main map zoom. Should always be below 0.
+basemap             | string   | "http://{s}.tile.osm.org/{z}/{x}/{y}.png"  | Leaflet tile layer [URL template](http://leafletjs.com/reference.html#url-template) (optional)
+basemapCredit       | string   | "Leaflet | © OpenStreetMap contributors"   | Credit for tilelayer. Goes at bottom, below note. (optional)
+creation            | boolean  | false                                      | Set to `true tot enables "creation mode" for use in admin tool.
+dragend             | function | undefined                                  | Anonymous function called on Leaflet `dragend` event (ie. when dragging/panning map).
+zoomend             | function | undefined                                  | Anonymous function called on Leaflet `zoomend` event (ie. when zooming).
+markerdragend       | function | undefined                                  | Anonymous function called on Leaflet marker `dragend` event (ie. when dragging a marker around the map). Only works when `creation` is true.
+
+
+### Marker options
+
+Markers are stored as an array of objects.
+
+option          | type   | default    | description
+--------------- | ------ | ---------- | ------------------
+lat             | number | (required) | Latitude of point.
+lon             | number | (required) | Longitude of point.
+text            | string | (blank)    | Text for marker callout.
+icon            | string | "square"   | Marker icon style: "circle", "square" or "none".
+label           | string | "callout"  | Marker label style: "callout" or "plain".
+label-direction | string | "north"    | Marker label direction. Available directions varies between label styles.
+
+### Example configuration JSON
+
+```json
+{
+    "aspect-ratio": "wide",
+    "dek": "",
+    "hed": "The U.K.",
+    "lat": 51.5049378,
+    "lon": -0.0870377,
+    "minimap": false,
+    "minimap-zoom-offset": -5,
+    "zoom": 4,
+    "markers": [
+        {
+            "icon": "circle",
+            "label": "plain",
+            "label-direction": "north",
+            "lat": 51.5049378,
+            "lon": -0.0870377,
+            "text": "London"
+        },
+        {
+            "icon": "circle",
+            "label": "plain",
+            "label-direction": "north",
+            "lat": 53.4779669,
+            "lon": -2.0613722,
+            "text": "Manchester"
+        }
+    ]
+}
+```
+
+### Public methods
+
+- `.remove()`: Reset map container to empty element.
+
+## Compiling dist folder
+
+You must have [npm](https://www.npmjs.com) and [grunt](http://gruntjs.com) installed.
+
+- `npm install`
+- `grunt`
+
+Note that the images in `src/img` are not auto-compiled - if they're changed, the SVG code will need to be copied into `pinpoint.js`.
+
+## Customising styles
+
+The look of the map and its markers is mostly controlled with CSS/SASS. These files can be found in `src/scss`:
+
+- **main:** Head and deck, scale line style
+- **overlay:** "Zoom/pan" icon
+- **markers:** Square, circle and plain/invisible marker styles
+- **marker-label-callout:** Callout label with north and south styles
+- **marker-label-plain:** Plain label with N, NE, E, SE, S, SW, W and NW styles
+- **geojson:** Styles for geojson objects
+
+To change the map's basemap/tilelayer, use the `basemap` configuration variable. See above for more details.
+
+## Changelog
+
+v1.0.0
+
+- Initial release
+
+
---
+++ src/js/pinpoint.js
@@ -0,0 +1,324 @@
+/*
+    Pinpoint v1.0.0
+*/
+
+function Pinpoint(opts){
+    'use strict';
+
+    var that = this;
+    this.opts = opts;
+    this.opts.el = this.opts.el || '#map-el';
+    this.$el = $(this.opts.el);
+    this.opts.basemap = opts.basemap || 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+    this.opts.basemapCredit = opts.basemapCredit || '<a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a> | &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+    
+    this.addElements();
+    this.fillText();
+    this.setAspectRatio();
+    this.setupMap();
+    this.calcBounds();
+    
+    this.disableInteraction();
+    
+    if (this.opts.creation) {
+        this.enableInteraction();
+        this.map.remove();
+        this.map.setMaxBounds( null );
+        this.setupMap({nozoomlimits: true});
+        
+    }
+
+    this.$el.find('.map-outer').click( this.enableInteraction.bind(that) );
+    
+    if (opts.minimap) {
+        this.addMinimap();
+    }
+    
+    for (var i = 0; i < opts.markers.length; i++) {
+        this.addMarker(opts.markers[i], i);
+    }
+    
+    console.log(opts);
+    if (opts.geojson) {
+        this.addGeoJSON(opts.geojson);
+    }
+    
+    if (typeof Iframe !== 'undefined') {
+        var fm = Iframe.init();
+    }
+    if ($(window).smartresize) {
+        $(window).smartresize( this.onWindowResize.bind(that) );
+    }
+}
+
+Pinpoint.prototype.addElements = function(){
+    
+    var pointer = '<!-- Generator: Adobe Illustrator 16.0.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  --><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg version="1.1" id="Layer_5" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"	 width="13.312px" height="15.267px" viewBox="0 0 13.312 15.267" enable-background="new 0 0 13.312 15.267" xml:space="preserve"><path fill="#697379" d="M13.312,6.4c0.002-1.229-2.215-1.265-2.203,0.008c0,0,0-0.604,0-0.846c0.016-1.146-2.188-1.216-2.2,0.007	v-0.44c0-1.251-2.307-1.277-2.307,0.007c0,0,0-2.586,0-4.218c0-1.203-2.341-1.246-2.341,0.005v7.485L2.049,6.199	C1.292,5.286-0.58,6.604,0.177,7.68l5.129,6.233c0,0,1.097,1.354,3.285,1.354c1.963,0,2.704-0.394,3.481-1.2	c0.754-0.78,1.24-1.657,1.24-2.938C13.312,10.76,13.312,7.192,13.312,6.4z"/></svg>';
+    var html = '<p class="pinpoint-hed"></p>'+
+    '<p class="pinpoint-dek"></p>'+
+    '<div class="map-outer inactive">'+
+        '<span class="map-cover"></span>'+
+        '<span class="image-interactive-icon">'+pointer+'<span class="image-interactive-icon-text">Zoom/Pan</span></span>'+
+        '<span class="image-interactive-icon image-interactive-icon-hover">'+pointer.replace('#697379','white')+'<span class="image-interactive-icon-text">Zoom/Pan</span></span>'+
+        '<div class="map-inner"></div>'+
+   ' </div>'+
+   ' <p class="pinpoint-note"></p>'+
+   ' <p class="pinpoint-source">'+this.opts.basemapCredit+'</p>';
+    this.$el.html( html );
+}
+
+Pinpoint.prototype.setAspectRatio = function(){
+    
+    var aspectRatios = {
+        "tall": 1.2,
+        "square": 1,
+        "wide": (2/3)
+    };
+    
+    var aspectRatio = aspectRatios[this.opts['aspect-ratio']];
+    var newHeight = this.$el.find('.map-inner').width() * aspectRatio;
+    this.$el.find('.map-inner, .map-cover').height( newHeight );
+    
+    if (this.map) {
+        this.map.invalidateSize();
+    }
+        
+}
+
+Pinpoint.prototype.onWindowResize = function(){
+    this.setAspectRatio();
+    $(window).trigger('resize');
+    // this.map.setView([this.opts.lat, this.opts.lon]);
+}
+
+Pinpoint.prototype.setupMap = function(mapopts){
+    
+    var opts = this.opts;
+    
+    var maxZoom = opts.zoom +1;
+    var minZoom = opts.zoom -1;
+    if (mapopts && mapopts.nozoomlimits) {
+        maxZoom = 20;
+        minZoom = 1;
+    }
+
+	var mapOptions = {
+		scrollWheelZoom: true,
+		keyboard: false,
+        maxZoom: maxZoom,
+        minZoom: minZoom,
+        attributionControl: false
+	};
+    
+    var mapEl = this.$el.find('.map-inner')[0];
+	this.map = L.map(mapEl, mapOptions)
+		.setView([opts.lat, opts.lon], opts.zoom);
+    L.control.scale({ position: 'topright' }).addTo(this.map); // scale bar
+    // put miles on top of km
+    var $scaleParent = this.$el.find('.leaflet-control-scale.leaflet-control');
+    $scaleParent.find('.leaflet-control-scale-line').eq(0).detach().appendTo($scaleParent);
+
+	L.tileLayer(this.opts.basemap).addTo(this.map);
+    
+    if (opts.dragend) {
+        this.map.on('dragend',opts.dragend);
+    }
+    if (opts.zoomend) {
+        this.map.on('zoomend',opts.zoomend);
+    }
+    
+}
+    
+Pinpoint.prototype.addMarker = function(mopts, index){
+    this.markers = this.markers || [];
+    mopts.icon = mopts.icon || 'circle'; 
+    // set the icon type
+	var squareIcon = L.divIcon({
+        className: 'marker-icon marker-icon-'+mopts.icon
+    });
+	var icon = L.marker(
+		[mopts.lat, mopts.lon],
+		{
+            icon: squareIcon,
+            draggable: this.opts.creation,
+            title: index
+		}
+	).addTo(this.map);
+    mopts.label = mopts.label || 'plain'; 
+    var labelDir = mopts["label-direction"] || 'north';
+    if (mopts.label === 'callout') {
+        var textBox = L.divIcon({
+            className: 'marker-label-callout '+labelDir,
+            html: '<div class="marker-inner">'+mopts.text+'</div>'
+        });
+    } else if (mopts.label === 'plain') {
+        var textBox = L.divIcon({
+            className: 'marker-label-plain '+labelDir,
+            html: '<div class="marker-inner">'+mopts.text+'</div>'
+        });
+    }
+	var text = L.marker(
+		[mopts.lat, mopts.lon],
+		{
+            icon: textBox
+		}
+	).addTo(this.map);
+    
+    // you can set .my-div-icon styles in CSS
+
+    // L.marker([50.505, 30.57]).addTo(map);
+    
+    var $mi = this.$el.find('.marker-inner').eq(-1);
+	$mi.css( 'margin-left', -$mi.outerWidth()/2 );
+	setTimeout((function(){
+    		$mi.css( 'margin-left', -$mi.outerWidth()/2 );
+	}).bind(this),100);
+    
+    if (this.opts.markerdragend) {
+        icon.on('dragend', this.opts.markerdragend);
+    }
+    
+    this.markers.push({
+        icon: icon,
+        text: text
+    });
+}
+
+Pinpoint.prototype.addMinimap = function(){
+	var minitiles = L.tileLayer(
+	this.opts.basemap, {
+		attribution: ''
+	});
+
+    var miniMap = new L.Control.MiniMap(minitiles, {
+        zoomLevelOffset: this.opts['minimap-zoom-offset'] || -5,
+        width: 100,
+        height: 100,
+        aimingRectOptions: {
+            color: 'black',
+            weight: 1,
+            opacity: 1,
+            fillColor: '#999',
+            fillOpacity: 0
+        }
+    }).addTo(this.map);
+    
+    // Disable drag and zoom handlers.
+    miniMap._miniMap.dragging.disable();
+    miniMap._miniMap.touchZoom.disable();
+    miniMap._miniMap.doubleClickZoom.disable();
+    miniMap._miniMap.scrollWheelZoom.disable();
+
+    // Disable tap handler, if present.
+    if (miniMap._miniMap.tap) miniMap._miniMap.tap.disable();
+    
+            
+}
+
+Pinpoint.prototype.calcBounds = function(){
+    var that = this;
+    
+    var bounds = calcNewMaxBounds(this.map);    
+    this.map.setMaxBounds( bounds );
+    
+    function calcNewMaxBounds(map){
+        // add an extra 30% margin to bounds
+        var boundScaleFactor = 1/3;
+        
+        var current = map.getBounds();
+        var hori = Math.abs( current._southWest.lng - current._northEast.lng ) * boundScaleFactor;
+        var vert = Math.abs( current._southWest.lat - current._northEast.lat ) * boundScaleFactor;
+        var southWest = {
+            lat: current._southWest.lat - vert,
+            lng: current._southWest.lng - hori
+        }
+        var northEast = {
+            lat: current._northEast.lat + vert,
+            lng: current._northEast.lng + hori
+        }
+        return L.latLngBounds(southWest, northEast);
+        
+    }
+}
+
+Pinpoint.prototype.fillText = function(){
+    if (this.opts.hed && (this.opts.hed.length > 0)) {
+        this.$el.find('.pinpoint-hed').text( this.opts.hed );
+    } else {
+        this.$el.find('.pinpoint-hed').hide();
+    }
+    if (this.opts.dek && (this.opts.dek.length > 0)) {
+        this.$el.find('.pinpoint-dek').text( this.opts.dek );
+    } else {
+        this.$el.find('.pinpoint-dek').hide();
+    }
+    if (this.opts.note && (this.opts.note.length > 0)) {
+        this.$el.find('.pinpoint-note').html( this.opts.note );
+    } else {
+        this.$el.find('.pinpoint-note').hide();
+    }
+    this.$el.find('.pinpoint-hed:visible(), .pinpoint-dek:visible()').eq(0).addClass('pinpoint-topline');
+}
+
+Pinpoint.prototype.disableInteraction = function(){
+    var map = this.map;
+    this.$el.find('.map-outer').addClass('inactive');
+    // map.dragging.disable();
+    // map.touchZoom.disable();
+    // map.doubleClickZoom.disable();
+    // // map.scrollWheelZoom.disable();
+    // map.boxZoom.disable();
+    // map.keyboard.disable();
+    return this;
+}
+
+Pinpoint.prototype.enableInteraction = function(){
+    var map = this.map;
+    this.$el.find('.map-outer').removeClass('inactive');
+    // map.dragging.enable();
+    // map.touchZoom.enable();
+    // map.doubleClickZoom.enable();
+    // // map.scrollWheelZoom.enable();
+    // map.boxZoom.enable();
+    // map.keyboard.enable();
+    return this;
+}
+
+Pinpoint.prototype.remove = function(){
+    this.map.remove();
+    this.$el.empty();
+}
+
+Pinpoint.prototype.addGeoJSON = function(geojson){
+    var map = this.map;
+    var features = geojson.features;
+    console.log(features);
+    for (var i = 0; i < features.length; i++) {
+        if (features[i].geometry && features[i].geometry) {
+            if (features[i].properties && features[i].properties.pinpointStyle) {
+                var styleName = features[i].properties.pinpointStyle;
+            } else {
+                var styleName = '';
+            }
+            var type = features[i].geometry.type;
+            if ( isOneOf( type, ["LineString", "MultiLineString", "Polygon", "MultiPolygon"]) ) {
+                L.geoJson(features[i].geometry, {
+                    style: {
+                        className: 'pinpoint-geojson '+styleName
+                    }
+                }).addTo(map);
+            
+            }
+        }
+    }
+    
+    function isOneOf( needle, haystack ){
+        for (var i = 0; i < haystack.length; i++) {
+            if (haystack[i] === needle) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
